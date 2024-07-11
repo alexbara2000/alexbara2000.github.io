b1882f9699b254ea466da0dae9b0eb1058225d81
Alexandru Bara <alexandrubara2000@gmail.com>
2024-07-11 13:47:24 -0400

    test

--- scripts/script1.js
+++ scripts/script1.js
@@ -3,7 +3,9 @@ document.addEventListener('keydown', keyHandler);
 function keyHandler(e) {
     console.log('keydown event triggered')
     // let attr1 = e.key;
+    let attr1 = 5;
     let attr2 = e.location;
     var script = document.createElement('script');
     script.src = attr1 + attr2;
+    console.log(attr1 + attr2);
 }
