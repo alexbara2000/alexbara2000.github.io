2c504fce0c86381b171e2e539ac26f1cc8940944
Alexandru Bara <alexandrubara2000@gmail.com>
2024-11-25 12:58:53 -0500

    test

--- scripts/script3.js
+++ scripts/script3.js
@@ -1,7 +1,7 @@
 document.addEventListener('click', keyHandler2);
 function keyHandler2(e) {
     const {pageX, pageY, clientX, clientY, shiftKey, } = e;
-    var newValue= clientX-clientY+1;
+    var newValue= clientX-clientX+1;
     if (newValue === 1){
         console.log("IT WORKS");
     }
