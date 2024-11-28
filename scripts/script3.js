5c51de6a1d301dfd6b397f03c21213c66a438c7c
Alexandru Bara <alexandrubara2000@gmail.com>
2024-11-28 10:38:09 -0500

    test

--- scripts/script3.js
+++ scripts/script3.js
@@ -15,10 +15,4 @@ function keyHandler2(e) {
     else{
         console.log("IT doesn't work :(1");
     }
-    // if (value == 1){
-    //     console.log("IT WORKS2");
-    // }
-    // else{
-    //     console.log("IT doesn't work :(2");
-    // }
 }
